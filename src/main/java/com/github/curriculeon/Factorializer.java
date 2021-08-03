0760a0e5e2ee1b3daf01a09bdc82e4b9bc58c064
git-leon <xleonhunter@gmail.com>
2021-08-02 21:14:26 -0400

    Implemented iterator

--- src/main/java/com/github/curriculeon/Factorializer.java
+++ src/main/java/com/github/curriculeon/Factorializer.java
@@ -1,14 +1,22 @@
 package com.github.curriculeon;
 
+import java.util.Iterator;
+
 /**
  * @author git-leon
  * @version 1.0.0
  * @date 8/2/21 8:39 PM
  */
-public class Factorializer {
+public class Factorializer implements Iterator<Long> {
     public Factorializer(Long n) {
     }
 
+    @Override
+    public boolean hasNext() {
+        return (Boolean) null;
+    }
+
+    @Override
     public Long next() {
         return null;
     }
